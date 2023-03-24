457e98e6f0628be603dc56760ec56ff04c75e961
Chipster <go@w.ay>
2023-03-24 13:07:03 -0500

    Update URL and json record_path for new dvref

--- UpdateMrefdHosts.py
+++ UpdateMrefdHosts.py
@@ -18,7 +18,7 @@ import pandas as pd
 
 # Parse command line options
 parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
-parser.add_argument('-u', '--url', default='https://reflectors.m17.link/ref-list/json', help="URL to lookup mrefd reflector JSON")
+parser.add_argument('-u', '--url', default='https://dvref.com/mrefd/json/?format=json', help="URL to lookup mrefd reflector JSON")
 parser.add_argument('-p', '--path', default=os.getcwd(), help="Path to save the hosts file (Defaults to current working directory)")
 parser.add_argument('-o', '--outfile', default="M17Hosts.txt", help="Output file name")
 args = vars(parser.parse_args())
@@ -34,7 +34,7 @@ headers = {
 }
 response = requests.request("GET", url, headers=headers).json()
 
-data = pd.json_normalize(response, record_path=['items'])
+data = pd.json_normalize(response, record_path=['reflectors'])
 df = data[['designator','ipv4','port']]
 df = df.assign(designator = 'M17-' + df.designator)
 df.to_csv(fullpath, sep=' ', index=False, header=False)
